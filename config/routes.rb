be57a96247deb85233c3a8d1c43046406c0e766c
watepu <40508321+watepu@users.noreply.github.com>
2018-09-15 19:22:35 +0900

    登録フォーム作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/' => "home#top"
 
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
