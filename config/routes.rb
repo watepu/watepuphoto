df36cb5b4a75e6e46a982a449a0eb671f33f2b30
watepu <40508321+watepu@users.noreply.github.com>
2018-09-16 09:24:36 +0900

    sessionコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   get '/' => "home#top"
 
   resources :users
+  resources :sessions
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
