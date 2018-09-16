43ab2bdd157ae7bddfe80e43731dbf6a1575bb7a
watepu <40508321+watepu@users.noreply.github.com>
2018-09-16 17:51:43 +0900

    user/editビュー、コントローラー追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "ユーザー情報を更新しました"
+      redirect_to user_path(@user.id)
+    else
+      flash[:danger] = "ユーザー情報の更新が出来ませんでした"
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
