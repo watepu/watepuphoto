7cc9512f3947ae7dcddaf0382874c8191c45ae63
watepu <40508321+watepu@users.noreply.github.com>
2018-09-17 12:49:40 +0900

    多対多の関係性追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_secure_password
   mount_uploader :icon, ImageUploader
   has_many :pictures, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_pictures, through: :favorites, source: :picture
 end
