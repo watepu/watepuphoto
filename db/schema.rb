c7ac8639796152fe34bb6fff670296263d3811bb
watepu <40508321+watepu@users.noreply.github.com>
2018-09-21 01:10:03 +0900

    マーグレーションファイル再作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180917033929) do
+ActiveRecord::Schema.define(version: 20180920160413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
