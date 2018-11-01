768e15121b753903580acb93358d3f04cecc758b
watepu <40508321+watepu@users.noreply.github.com>
2018-11-01 12:39:10 +0900

    rspec詳細入力

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,8 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  it "is valid with a title, production_year, running_time, rating"
-  it "is invalid without a title"
-  it "is invalid with a duplicate title"
-  it "is invalid with title is 101 or more characters"
+  it "is valid with a title, production_year, running_time, rating" do
+    movie = Movie.new(
+      title: 'DIC',
+      production_year: 2000,
+      running_time: 120,
+      rating: 'G'
+    )
+    expect(movie).to be_valid
+  end
+  it "is invalid without a title" do
+    movie = Movie.new(title: nil)
+    movie.valid?
+    expect(movie.errors[:title]).to include("を入力してください")
+  end
+  it "is invalid with a duplicate title" do
+    Movie.create(
+      title: 'DIC',
+      production_year: 2000,
+      running_time: 120,
+      rating: 'G'
+    )
+    movie = Movie.new(
+      title: 'DIC',
+      production_year: 3000,
+      running_time: 150,
+      rating: 'PG12'
+    )
+    movie.valid?
+    expect(movie.errors[:title]).to include("はすでに存在します")
+  end
+  it "is invalid with title is 101 or more characters" do
+    movie = Movie.new(title: 'a'*101)
+    movie.valid?
+    expect(movie.errors[:title]).to include("は100文字以内で入力してください")
+  end
 end
